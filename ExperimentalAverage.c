28ce84892e107e72502b4e93252ccc9e487b59c8
Anthony MIRABEAU <amirab1@lab2-18.CS.McGill.CA>
2014-11-14 11:15:10 -0500

    git commit test

---
+++ ExperimentalAverage.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stlib.h>
+
+int main(int argc, char *argv[]){
+
+    if(argc!=2){
+        printf("%s", "Incorrect format of arguments. Please enter : ExperimentalAverage Filename");
+	return EXIT_FAILURE;
+    }
+
+    
+
+
+}
